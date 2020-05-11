4835c1f174ce74b8cfd76d114e04709e63118f8b
Ashish Khatiwada <ashkhatnp@gmail.com>
2020-05-11 09:26:16 +0545

    First commit

---
+++ SeleniumJob/WebToExcel/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using OfficeOpenXml;
+
+namespace WebToExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IterateExcel(2, 30000);
+
+        }
+        public static void IterateExcel(int Column, int Row)
+        {
+            string path = "F:\\Selenium\\SeleniumJob\\Data\\AshishWorksheet.xlsx";
+            MemoryStream stream = new MemoryStream();
+            ExcelPackage package = new ExcelPackage(stream);
+            //var sheet = package.Workbook.Worksheets.Add("Ashish");
+            var shee = package.Workbook
+            sheet.Cells[1, 1].Value = "Name";
+            sheet.Cells[1, 2].Value = "Addres111s";
+            package.Save();
+
+
+
+            // for (int i = 2; i <= Row; i++)
+            // {
+            //     for (int j = 1; j <= Column; j++)
+            //     {
+            //         Console.WriteLine("Starting the process");
+            //         var stream = new MemoryStream();
+            //         using (var package = new ExcelPackage(stream))
+            //         {
+            //             var sheet = package.Workbook.Worksheets.Add("ashishTest");
+            //         }
+            //     }
+            // }
+        }
+    }
+}
---
+++ firstProj/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace firstProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IWebDriver driverChrome = new ChromeDriver();
+            driverChrome.Navigate().GoToUrl("https://www.facebook.com/ashish.khatiwada.710");
+            driverChrome.FindElement(By.Id("email")).SendKeys("ashish.khatiwada.710");
+            driverChrome.FindElement(By.XPath("//input[@id='pass']")).SendKeys("H2oc#.nett");
+            driverChrome.FindElement(By.XPath("//input[@id='pass']")).SendKeys(Keys.Return);
+
+            driverChrome.Navigate().GoToUrl("https://www.facebook.com/ashish.khatiwada.710");
+            driverChrome.FindElement(By.ClassName("_1frb")).SendKeys(Keys.Control + "a");
+            driverChrome.FindElement(By.ClassName("_1frb")).SendKeys(Keys.Delete);
+            driverChrome.FindElement(By.ClassName("_1frb")).SendKeys("suraj subedi");
+            driverChrome.FindElement(By.ClassName("_1frb")).SendKeys(Keys.Return);
+
+
+            //Mozilla Driver
+            // IWebDriver driverFirefox = new FirefoxDriver();
+            // driverFirefox.Navigate().GoToUrl("https://www.facebook.com");
+            // driverFirefox.FindElement(By.Id("email")).SendKeys("ashish.khatiwada.710");
+            // driverFirefox.FindElement(By.Id("pass")).SendKeys("H2oc#.nett");
+            // driverFirefox.FindElement(By.Id("pass")).SendKeys(Keys.Return);
+            // driverFirefox.Navigate().GoToUrl("https://www.facebook.com/ashish.khatiwada.710");
+            // driverFirefox.FindElement(By.ClassName("_1frb")).SendKeys("Asmita Khatiwada");
+            // driverFirefox.FindElement(By.ClassName("_1frb")).SendKeys(Keys.Return);
+
+        }
+    }
+}
